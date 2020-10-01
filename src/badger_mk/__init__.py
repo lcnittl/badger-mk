d73ed6144361dfb10ca5463eb45b35be37ad2902
Christian Knittl-Frank <lcnittl@gmail.com>
2020-10-01 21:35:19 +0200

    Remove comment header

--- src/badger_mk/__init__.py
+++ src/badger_mk/__init__.py
@@ -1,24 +1,4 @@
 #!/usr/bin/env python3
-#
-# badger - an Inkscape extension to export images with automatically replaced values
-# Copyright (C) 2008  Aurélio A. Heckert (original Generator extension in Bash)
-#               2019  Maren Hachmann (Python rewrite, update for Inkscape 1.0)
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-#
-# Version 0.9
 """
 An Inkscape extension to automatically replace values (text, attribute values)
 in an SVG file and to then export the result to various file formats.
